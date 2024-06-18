b4ae085bfe6691a2fdf5b547cbf9df3773202cfc
antal <antal.horvath.90@gmail.com>
2024-06-18 20:03:09 +0200

    config: bootstrap app module to configure auth0

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,8 @@
 import {Component, OnInit} from '@angular/core';
-import {RouterOutlet} from '@angular/router';
 import {initFlowbite} from 'flowbite';
-import {NavbarComponent} from "./core/components/navbar/navbar.component";
 
 @Component({
   selector: 'app-root',
-  standalone: true,
-  imports: [RouterOutlet, NavbarComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
