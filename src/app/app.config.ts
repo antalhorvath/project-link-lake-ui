9fecb48756aac1d7c1f64db005e3adc3844f9dc8
antal <antal.horvath.90@gmail.com>
2024-06-19 20:09:02 +0200

    tech: add ngrx devtools

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,4 +1,4 @@
-import {ApplicationConfig, importProvidersFrom, provideZoneChangeDetection} from '@angular/core';
+import {ApplicationConfig, importProvidersFrom, isDevMode, provideZoneChangeDetection} from '@angular/core';
 import {provideRouter} from '@angular/router';
 
 import {routes} from './app.routes';
@@ -9,6 +9,7 @@ import {provideStore} from '@ngrx/store';
 import {metaReducers, reducers} from './reducers';
 import {provideEffects} from '@ngrx/effects';
 import {provideRouterStore} from '@ngrx/router-store';
+import {provideStoreDevtools} from '@ngrx/store-devtools';
 
 export const appConfig: ApplicationConfig = {
   providers: [
@@ -29,6 +30,7 @@ export const appConfig: ApplicationConfig = {
     },
     provideStore(reducers, {metaReducers}),
     provideEffects(),
-    provideRouterStore()
+    provideRouterStore(),
+    provideStoreDevtools({maxAge: 25, logOnly: !isDevMode()})
   ]
 };
